a4eeef1d3e1778f6cdca4aa0c60a2f650b891430
Moritz Hasse <Khyron83@gmail.com>
2018-02-07 18:12:41 +0100

    + added dapper benchmark

--- Sql2Json.Benchmark/Program.cs
+++ Sql2Json.Benchmark/Program.cs
@@ -18,19 +18,22 @@ using System.Text;
 namespace Sql2Json.Benchmark
 {
     class Program
-    {
+    {   
+        static int total = 100;
+        static int pageSize = 15;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Running Benchmarks");
             //   NpgsqlLogManager.Provider = new ConsoleLoggingProvider(NpgsqlLogLevel.Debug, true, true);
-            const int size = 500;
-            CreateTables(size, size * 1);
+            Console.WriteLine("Creating tables and preparing test data");
+            CreateTables(total, 0);
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("----------------------------------");
                 Console.WriteLine("Round " + (i + 1));
                 RunNativSqlBenchmark();
-            //    RunDapperBenchmark();
+                RunDapperBenchmark();
                 RunEfBenchmark();
                 RunMappingEngineNestedResultsBenchmark();
                 RunMappingEngineWithSubSelectsBenchmark();
@@ -73,14 +76,15 @@ namespace Sql2Json.Benchmark
             command.CommandText = "DELETE FROM order_items";
             command.ExecuteNonQuery();
 
+            Random random = new Random();
 
             for (int i = idOffset; i < entries + idOffset; i++)
             {
                 command.CommandText = string.Format("INSERT INTO customer (id, sname,fname,BDAY,ADR_STREET,ADR_CITY)  VALUES({0}, '{1}','{2}','{3}', '{4}', '{5}')", i + 1, "Jens", "Müller", "1988-10-31", "Street " + i.ToString(), "Los Angeles");
                 command.ExecuteNonQuery();
-                for (int k = 0; k < 4; k++)
+                for (int k = 0; k < 9; k++)
                 {
-                    command.CommandText = string.Format("INSERT INTO invoices (id, inv_date,amount,customer_id) VALUES({0}, '{1}',{2},{3})", (i + 1).ToString() + (k + 1).ToString(), "2017-12-12", i*2, i + 1);
+                    command.CommandText = string.Format("INSERT INTO invoices (id, inv_date,amount,customer_id) VALUES({0}, '{1}',{2},{3})", (i + 1).ToString() + (k + 1).ToString(), "2017-12-12", i * 2, i + 1);
                     command.ExecuteNonQuery();
                 }
 
@@ -89,10 +93,10 @@ namespace Sql2Json.Benchmark
                     command.CommandText = string.Format("INSERT INTO orders (id, ordernumber, article, customer_id) VALUES({0}, '{1}','{2}',{3})", (i + 1).ToString() + (j + 1).ToString(), "O-155522-GGGG", "USB STICK", i + 1);
                     command.ExecuteNonQuery();
 
-                    for (int l = 0; l < 4; l++)
+                    for (int l = 0; l < 7; l++)
                     {
                         var oiid = (i + 1).ToString() + (j + 1).ToString() + (l + 1).ToString();
-                        command.CommandText = string.Format("INSERT INTO order_items (id, item_name, price, order_id) VALUES({0}, '{1}',{2},{3})", oiid, "Book abc - " + oiid, 4899, (i + 1).ToString() + (j + 1).ToString());
+                        command.CommandText = string.Format("INSERT INTO order_items (id, item_name, price, order_id) VALUES({0}, '{1}',{2},{3})", oiid, "Notebook model - " + oiid, random.Next(1000, 2000), (i + 1).ToString() + (j + 1).ToString());
                         command.ExecuteNonQuery();
                     }
 
@@ -134,6 +138,7 @@ namespace Sql2Json.Benchmark
                         .Column("OrderId", "oid")
                         .Column("ArticleName", "article")
                         .NestedResults("Items", "oiid", cfg3 => cfg3
+                             .Column("id", "oiid")
                              .Column("Item_Name")
                              .Column("Price")
                         )
@@ -149,10 +154,10 @@ namespace Sql2Json.Benchmark
             using (var engine = new MappingEngine(() => new NpgsqlConnection("Server=localhost;User Id=postgres;" +
                                "Password=admin;Database=test;"), mapping))
             {
-                using (var ms = new FileStream("out1.json", FileMode.Create))
+                using (var ms = new FileStream("out-sql2json-nested.json", FileMode.Create))
                 {
                     var context = new Dictionary<string, object>();
-              //      context.Add("min_amount", 10);
+                    //      context.Add("min_amount", 10);
                     engine.ExecuteMapping(ms, context, false);
                 }
             }
@@ -185,7 +190,8 @@ namespace Sql2Json.Benchmark
                    .Query("Orders", "select o.article, o.id as oid from orders o where o.customer_id=${column:cid:Int32}", cfg2 => cfg2
                         .Column("OrderId", "oid")
                         .Column("ArticleName", "article")
-                        .Query("Items", "select oi.item_name, oi.price from order_items oi where oi.order_id=${column:oid:Int32}", cfg3 => cfg3
+                        .Query("Items", "select oi.id, oi.item_name, oi.price from order_items oi where oi.order_id=${column:oid:Int32}", cfg3 => cfg3
+                            .Column("id")
                             .Column("item_name")
                             .Column("price")
                         )
@@ -202,7 +208,7 @@ namespace Sql2Json.Benchmark
             using (var engine = new MappingEngine(() => new NpgsqlConnection("Server=localhost;User Id=postgres;" +
                                "Password=admin;Database=test;"), mapping))
             {
-                using (var ms = new FileStream("out3.json", FileMode.Create))
+                using (var ms = new FileStream("out-sql2json-subselect.json", FileMode.Create))
                 {
                     var context = new Dictionary<string, object>();
                     //  context.Add("min_amount", 100);
@@ -227,7 +233,7 @@ namespace Sql2Json.Benchmark
                 @"select row_to_json(cu) 
 	                    from (select c.id as cid, c.sname,'2017-03-01' as LastUpdate, c.fname, c.BDAY, c.ADR_STREET, c.ADR_CITY,
                           (select json_agg(inv) from (select i.id, i.inv_date, i.amount from invoices as i  where i.customer_id=c.id) as inv  ) as invoices,
-                          (select json_agg(ord) from (select o.id, o.article ,      (select json_agg(itm) from (select oi.item_name, oi.price from order_items as oi  where oi.order_id=o.id) as itm  ) as items          from orders as o  where o.customer_id=c.id) as ord  ) as orders
+                          (select json_agg(ord) from (select o.id, o.article ,      (select json_agg(itm) from (select oi.id, oi.item_name, oi.price from order_items as oi  where oi.order_id=o.id) as itm  ) as items          from orders as o  where o.customer_id=c.id) as ord  ) as orders
                     from customer c) as cu";
             StringBuilder sb = new StringBuilder();
             sb.Append("[");
@@ -240,11 +246,28 @@ namespace Sql2Json.Benchmark
                 }
             }
             sb.Append("]");
-            File.WriteAllText("ou2.json", sb.ToString());
+            File.WriteAllText("out-nativesql.json", sb.ToString());
             command.Dispose();
             sw.Stop();
             Console.WriteLine("RunNativSqlBenchmark time elapsed: " + sw.ElapsedMilliseconds);
             conn2.Close();
+        }
+
+
+
+        private static Tuple<string, IDictionary<string, object>> AppendIdsToQuery(string query, IEnumerable<int> ids)
+        {
+            Dictionary<string, object> paramObj = new Dictionary<string, object>();
+            List<string> paramNames = new List<string>();
+            int index = 0;
+            foreach (var id in ids)
+            {
+                var paramName = "@Id" + index;
+                paramObj.Add(paramName, id);
+                paramNames.Add(paramName);
+                index++;
+            }
+            return new Tuple<string, IDictionary<string, object>>(query + "(" + string.Join(",", paramNames) + ")", paramObj);
         }
 
         private static void RunDapperBenchmark()
@@ -256,39 +279,40 @@ namespace Sql2Json.Benchmark
                    "Password=admin;Database=test;");
             conn.Open();
 
-            var customers = conn.Query<Customer>(@"select c.id , c.sname,'2017-03-01' as LastUpdate, c.fname, c.BDAY, c.ADR_STREET, c.ADR_CITY
-                                    from customer c order by c.id").ToList();
-            var invoices = conn.Query<Invoice>("select i.id, i.amount, i.customer_id from invoices i Order by customer_id"/*, new { Id = customer.Id }*/).ToList();
-            var orders = conn.Query<Order>("select o.id, o.article, o.customer_id from orders o Order by customer_id"/*, new { Id = customer.Id }*/).ToList();
-            var orderItems = conn.Query<OrderItem>("select oi.item_name, oi.price, oi.order_id from order_items oi Order by oi.order_id" /*, new { Id = order.Id }*/).ToList();
-
-
-
-
-
-
-            int index = 0;
-            int key = invoices.ElementAt(index).customer_id;
-
-            foreach (var cust in customers)
+            File.WriteAllText("out-dapper.json", "");
+            var count = conn.Query<int>("select count(*) from customer");
+            for (int i = 0; i < count.First() / pageSize; i++)
             {
+                var customers = conn.Query<Customer>(@"select c.id , c.sname,'2017-03-01' as LastUpdate, c.fname, c.BDAY, c.ADR_STREET, c.ADR_CITY
+                                  from customer c order by c.id LIMIT @limit OFFSET @offset", new { limit = pageSize, offset = i * pageSize }).ToList();
+                var queryInvoices = AppendIdsToQuery("select i.id, i.amount, i.customer_id from invoices i where customer_id in ", customers.Select(c => c.Id));
+                var invoices = conn.Query<Invoice>(queryInvoices.Item1, queryInvoices.Item2).ToList();
 
-                while(key < cust.Id)
+                var queryOrders = AppendIdsToQuery("select o.id, o.article, o.customer_id from orders o where customer_id in ", customers.Select(c => c.Id));
+                var orders = conn.Query<Order>(queryOrders.Item1, queryOrders.Item2).ToList();
+
+                for (int k = 0; k < orders.Count / pageSize; k++)
                 {
-                    index++;
-                    key = invoices.ElementAt(index).customer_id;
+                    var ordersPage = orders.Skip(k * pageSize).Take(pageSize);
+                    var qi = AppendIdsToQuery("select oi.id, oi.item_name, oi.price, oi.order_id from order_items oi where oi.order_id in ", ordersPage.Select(o => o.Id));
+                    var orderItems = conn.Query<OrderItem>(qi.Item1, qi.Item2).ToList();
+                    foreach (var order in ordersPage)
+                    {
+                        order.Items = orderItems.Where(oi => oi.order_id == order.Id).ToList();
+                    }
                 }
-            //    if(key== cust.Id) cust.Invoices = grouped.ElementAt(index).vale
-
-
+                foreach (var cust in customers)
+                {
+                    cust.Invoices = invoices.Where(inv => inv.customer_id == cust.Id).ToList();
+                    cust.Orders = orders.Where(ord => ord.customer_id == cust.Id).ToList();
+                }
+                File.AppendAllText("out-dapper.json", JsonConvert.SerializeObject(customers));
             }
-
 
             conn.Close();
             conn.Dispose();
 
-
-            File.WriteAllText("out4.json", JsonConvert.SerializeObject(customers));
+            //     File.WriteAllText("out4.json", JsonConvert.SerializeObject(customers));
 
             sw.Stop();
             Console.WriteLine("RunDapperBenchmark time elapsed: " + sw.ElapsedMilliseconds);
@@ -298,9 +322,9 @@ namespace Sql2Json.Benchmark
         {
             var sw = new Stopwatch();
             sw.Start();
+            File.WriteAllText("out-ef.json", "");
 
-            var total = 10000;
-            var pageSize = 15;
+
             for (int i = 0; i < total / pageSize; i++)
             {
                 using (var context = new EfContext())
@@ -311,7 +335,8 @@ namespace Sql2Json.Benchmark
                         .ThenInclude(order => order.Items)
                         .Skip(i * pageSize).Take(pageSize)
                     .ToList();
-                    File.WriteAllText("out5" + i.ToString() + ".json", JsonConvert.SerializeObject(customers));
+                    File.AppendAllText("out-ef.json", JsonConvert.SerializeObject(customers));
+
                 }
             }
 
